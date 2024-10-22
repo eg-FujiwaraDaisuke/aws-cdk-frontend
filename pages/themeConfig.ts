27c8eee9ef04f14a6c5f402a2945799b4a9708b4
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-22 16:48:32 +0900

    add: themeConfig

---
+++ pages/themeConfig.ts
@@ -0,0 +1,11 @@
+const themeConfig = {
+    primaryColor: '#1976d2',
+    secondaryColor: '#dc004e',
+    backgroundColor: '#f5f5A4',
+    textColor: {
+      primary: '#000000',
+      secondary: '#ffffff',
+    },
+};
+
+export default themeConfig;
