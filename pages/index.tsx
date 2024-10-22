d869779733d908849fff8f95724f12e938c495b3
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-22 16:50:07 +0900

    edit: Button in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to My Next.js App!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
