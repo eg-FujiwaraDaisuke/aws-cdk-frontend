f96424130c7f8656f768a8f40f8a20f6a31da9eb
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-18 16:21:31 +0900

    separate: login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,15 @@
-import { Container, FormControl } from '@mui/material';
-import { signIn } from 'aws-amplify/auth';
-import { Formik } from 'formik';
-import React from 'react';
-import * as Yup from 'yup';
-import { ButtonComponents } from './components/button';
-import FormTextField from './components/inputField';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const Home = () => {
-  return (
-    <Container maxWidth="md">
-        <h2>Login</h2>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validationSchema={Yup.object({
-            email: Yup.string()
-              .email('Invalid email address')
-              .required('Required'),
-            password: Yup.string()
-              .min(8, 'Password must be at least 8 characters')
-              .required('Required'),
-          })}
-          onSubmit={async (values) => {
-            try {
-              await signIn({ username: values.email, password: values.password});
-              alert('Login successful!');
-            } catch (err) {
-              console.error(err);
-              alert('Login failed. Please check your credentials.');
-            }
-          }}
-        >
-          {formik => (
-            <form onSubmit={formik.handleSubmit}>
-              <FormControl>
-                <FormTextField
-                  label="Email"
-                  name="email"
-                  type="email"
-                />
-              </FormControl>
-              <FormControl>
-                <FormTextField
-                  label="Password"
-                  name="password"
-                  type="password"
-                />
-              </FormControl>
-                
-              <ButtonComponents type="submit" label="Login" />
-            </form>
-          )}
-        </Formik>
-    </Container>
-  );
+const IndexPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // コンポーネントがマウントされたときにリダイレクト
+    router.push('/login');
+  }, [router]);
+
+  return null; // 何も表示しない
 };
 
-export default Home;
+export default IndexPage;
